221c5aeb84f5e2ac6f79a12016fa84a0942cf2d0
Pramod Thomas <pmod23@gmail.com>
2020-05-27 18:34:34 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,37 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  
+  #populate new new_hash
+  data.each do |key, value|
+    value.each do |k, v|
+      v.each do |name|
+        
+      if !new_hash.key?(name)
+        new_hash[name] = {:color => [], :gender => [], :lives => []}
+      end
+      
+      end
+    end
+  end
+   data.each do |key, value|
+    value.each do |k, v|
+      v.each do |name|
+        
+    
+        if(key == :color)
+          new_hash[name][:color] << k.to_s
+          elsif(key == :gender)
+          new_hash[name][:gender] << k.to_s
+          elsif(key==:lives)
+          new_hash[name][:lives] << k.to_s
+          end
+         
+      end
+    end
+  end
+  
+  new_hash
+  
 end
+
+
